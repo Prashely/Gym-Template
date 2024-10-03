462f027a0957a68f7b142c1fb3e56283d4956f22
Prashely <danielprashely@gmail.com>
2024-10-03 18:12:09 +0200

    Y

--- src/assets/components/ui/Reasons.jsx
+++ src/assets/components/ui/Reasons.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Reasons = ({ imageSrc, title, description, icon }) => {
   return (
-    <div className="relative  w-[95vw] h-[450px] overflow-hidden rounded-lg shadow-lg">
-      <img src={imageSrc} alt={title} className="w-full h-full object-cover" />
-      {/* Black background on the lower half */}
+    <div className="relative w-[95vw] h-[450px] overflow-hidden rounded-lg shadow-lg">
+      <img
+        src={imageSrc}
+        alt={title}
+        className="w-full h-full object-cover"
+        loading="lazy" // Lazy load images
+      />
       <div className="absolute bottom-0 w-full h-[200px] bg-bgPrimary bg-opacity-90 p-4 flex flex-col items-center justify-center text-center text-white">
         <p className="text-4xl mb-2">{icon}</p>
         <h3 className="text-2xl font-bold">{title}</h3>
