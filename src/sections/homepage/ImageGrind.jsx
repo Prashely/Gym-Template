7ef7e92e8b1b5a6236c48adb9d9b8374e48a077c
Prashely <danielprashely@gmail.com>
2024-10-13 18:48:13 +0200

    image grid

--- src/sections/homepage/ImageGrind.jsx
+++ src/sections/homepage/ImageGrind.jsx
@@ -5,12 +5,12 @@ import Eight from "../../assets/images/8.jpg";
 
 const ImageGrid = () => {
   return (
-    <section className="w-full py-2 bg-white">
+    <section className="w-full py-2 bg-white lg:h-auto">
       <div className="container mx-auto px-4">
         {/* Flex container for two main grids */}
-        <div className="md:h-[500px] flex justify-center gap-4">
+        <div className="flex justify-center gap-4 lg:h-[400px]">
           {/* First Grid: Single Image */}
-          <div className="w-[40vw] min-w-0">
+          <div className="w-[40vw] lg:w-[25vw] min-w-0">
             <img
               src={Nine}
               alt="Image 1"
@@ -19,7 +19,7 @@ const ImageGrid = () => {
           </div>
 
           {/* Second Grid: Two Images Stacked */}
-          <div className="flex flex-col w-[40vw] gap-3">
+          <div className="flex flex-col lg:flex-row w-[50vw] gap-3">
             <img
               src={Ten}
               alt="Image 2"
@@ -28,7 +28,7 @@ const ImageGrid = () => {
             <img
               src={Eight}
               alt="Image 3"
-              className="w-full h-full object-cover rounded-lg shadow-lg"
+              className="w-full h-full object-cover rounded-lg shadow-lg lg:mb-100"
             />
           </div>
         </div>
